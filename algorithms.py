27fa0abcd0726f13f3ab0c12463cf137a106dce8
Alex <alexandertaylor315@gmail.com>
2023-03-25 15:07:05 -0400

    added algorithms.py file and solved even or odd and less than 100 task algorithm problems by building functions and give big o time complexity

---
+++ algorithms.py
@@ -0,0 +1,27 @@
+
+# time complexity is O(1)
+# def function(n):
+#     if (n % 2) == 0:
+#         return True
+#     else:
+#         return False
+        
+# print(function(2))
+
+
+# time complexity is O(n)
+# def function_list(x):
+#     i = 0
+#     while i < len(x):
+#         n = x[i]
+#         if n < 100:
+#             i += 1
+#         else:
+#             return False
+#     return True
+        
+# print(function_list([24, 58, 44, 6, 88]))
+
+
+
+    
